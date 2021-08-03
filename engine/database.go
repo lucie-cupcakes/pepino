692453ad081fc33d4400f7e72c02089645aa0b17
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-08-03 15:49:19 +0000

    PepinoEngine can now Save and Load data

--- engine/database.go
+++ engine/database.go
@@ -3,6 +3,7 @@ package pepinoengine
 import (
 	"bytes"
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"os"
 )
@@ -21,6 +22,8 @@ func (d *PepinoDatabase) New(name string) {
 
 // Save stores object state
 func (d *PepinoDatabase) Save() error {
+	//@TODO: the Database folder path should be absolute and stored
+	// into a config object or something.
 	var buff bytes.Buffer
 	enc := gob.NewEncoder(&buff)
 	err := enc.Encode(d.Entries)
@@ -31,12 +34,44 @@ func (d *PepinoDatabase) Save() error {
 	err = os.WriteFile("./data/"+d.Name, buff.Bytes(), 0644)
 	if err != nil {
 		return fmt.Errorf("error saving PepinoDatabase object:"+
-			"\n\tos.WriteFile error: %s", err.Error())
+			"\n\tos.WriteFile() error: %s", err.Error())
 	}
 	return nil
 }
 
+// HasSavedData tells whatever a PepinoDatabase object has saved data
+// available to restore
+func (d *PepinoDatabase) HasSavedData() bool {
+	if _, err := os.Stat("./data/" + d.Name); err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
 // Load restores object state
-func (d *PepinoDatabase) Load() {
-	fmt.Println("pepinoengine: TODO: Load()")
+func (d *PepinoDatabase) Load() error {
+	if !d.HasSavedData() {
+		return errors.New("error loading PepinoDatabase object:" +
+			"\n\tthe object does not have saved data")
+	}
+
+	var buff bytes.Buffer
+	fBytes, err := os.ReadFile("./data/" + d.Name)
+	if err != nil {
+		return fmt.Errorf("error loading PepinoDatabase object:"+
+			"\n\tos.ReadFile() error: %s", err.Error())
+	}
+	_, err = buff.Write(fBytes)
+	if err != nil {
+		return fmt.Errorf("error loading PepinoDatabase object:"+
+			"\n\tbuff.Write() error: %s", err.Error())
+	}
+	dec := gob.NewDecoder(&buff)
+	err = dec.Decode(&d.Entries)
+	if err != nil {
+		return fmt.Errorf("error loading PepinoDatabase object:"+
+			"\n\tgobDecoder.Decode() error: %s", err.Error())
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	pepinoengine "github.com/lucie-cupcakes/pepino/engine"
 )
 
 func main() {
 	var db pepinoengine.PepinoDatabase
 	db.New("test-db")
-	db.Entries["hello"] = []byte("Hello world!")
-	db.Save()
+	if db.HasSavedData() {
+		err := db.Load()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("The database has been loaded.")
+		fmt.Println("there are " + strconv.Itoa(len(db.Entries)) + " entries.")
+		for k, v := range db.Entries {
+			fmt.Println("key{" + k + "} = \"" + string(v) + "\"")
+		}
+	} else {
+		fmt.Println("The database has been created.")
+		db.Entries["hello"] = []byte("Hello world!")
+		err := db.Save()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("The database has been saved.")
+	}
 
 }
